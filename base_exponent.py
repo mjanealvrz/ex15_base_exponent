2201efde59a9d1dcb9b88a96e582e340d894a4f0
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 15:40:47 +0800

    Move downward the variable for base and exponent to call the function

--- base_exponent.py
+++ base_exponent.py
@@ -2,17 +2,19 @@
 # Note here exp is a non-negative integer, and the base is an integer.
 
 # Pseudocode
-# Set variable for base and exponent and store values
-base_value= 5
-exponent_value= 4
+
 
 # Create a function to calculate the exponentiation
-def exponent():
+def exponent(base_value, exponent_value):
     # Calculate the result of base raised to the power of exp
      result = base_value ** exponent_value
     # Return the calculated result
      return result 
+
+# Set variable for base and exponent and store values
+base_value = 5
+exponent_value = 4
 # Set result to call  exponent with parameters base value and exponent value
 result = exponent(base_value, exponent_value)
 # Print result
-print(f"The {base_value} raised to the power of {exponent_value} is: {result}")
+print(f"The {base_value} raised to the power of {exponent_value} is {result} .")
