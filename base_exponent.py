1d8520ae6ab2fee2b80af172551d7a9c2cf092a4
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 15:30:03 +0800

    Added program statement and created pseudocode

---
+++ base_exponent.py
@@ -0,0 +1,9 @@
+#  Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+# Note here exp is a non-negative integer, and the base is an integer.
+
+# Pseudocode
+# Set variable for base and exponent and store values
+# Create a function to calculate the exponentiation
+    # Calculate the result of base raised to the power of exp
+    # Return the calculated result
+# Print result
