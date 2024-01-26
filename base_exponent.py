8f456fc4ded0087af3f24c11d340d8ac509a5791
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 15:38:21 +0800

    Created function for exponentiation

--- base_exponent.py
+++ base_exponent.py
@@ -3,7 +3,16 @@
 
 # Pseudocode
 # Set variable for base and exponent and store values
+base_value= 5
+exponent_value= 4
+
 # Create a function to calculate the exponentiation
+def exponent():
     # Calculate the result of base raised to the power of exp
+     result = base_value ** exponent_value
     # Return the calculated result
+     return result 
+# Set result to call  exponent with parameters base value and exponent value
+result = exponent(base_value, exponent_value)
 # Print result
+print(f"The {base_value} raised to the power of {exponent_value} is: {result}")
