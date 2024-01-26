02eec0fe9f0c0f758e19298ba77faa628067647c
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 15:41:42 +0800

    Fixed spaces

--- base_exponent.py
+++ base_exponent.py
@@ -3,18 +3,21 @@
 
 # Pseudocode
 
-
 # Create a function to calculate the exponentiation
 def exponent(base_value, exponent_value):
+     
     # Calculate the result of base raised to the power of exp
      result = base_value ** exponent_value
+
     # Return the calculated result
      return result 
 
 # Set variable for base and exponent and store values
 base_value = 5
 exponent_value = 4
+
 # Set result to call  exponent with parameters base value and exponent value
 result = exponent(base_value, exponent_value)
+
 # Print result
 print(f"The {base_value} raised to the power of {exponent_value} is {result} .")
